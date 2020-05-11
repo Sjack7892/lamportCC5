0f04b263d395c8b152dbfcb35c23ed7dce0ae537
Steve Jackson <sjack7892@gmail.com>
2020-05-11 14:30:06 -0500

    speed control

--- src/components/SpeedControl/SpeedControl.js
+++ src/components/SpeedControl/SpeedControl.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 // THIS COMPONENT IS OUR INTERFACE FOR SPEED
 // YOU SHOULD DISPLAY THE CURRENT SPEED
 // BUTTONS SHOULD INCREASE OR DECREASE SPEED, RESPECTIVELY
 
 class SpeedControl extends Component {
+
+  handleClickIncrease = () => {
+    console.log('speed increased');
+    this.props.dispatch({
+      type: 'increaseSpeed',
+      payload: 1
+  })
+  }
+
+  handleClickDecrease = () => {
+    console.log('speed decreased');
+    this.props.dispatch({
+      type: 'decreaseSpeed',
+      payload: 1
+  })
+  }
+
   render() {
     return (
       <div>
         <h2>Speed Control</h2>
 
-        <button>Increase Speed</button>
-        <p>SPEED: GOES HERE</p>
-        <button>Decrease Speed</button>
+        <button onClick={this.handleClickIncrease}>Increase Speed</button>
+        <p>SPEED: {this.props.reduxState}</p>
+        <button onClick={this.handleClickDecrease}>Decrease Speed</button>
       </div>
     )
   }
 }
 
-export default SpeedControl;
+const reduxStateToProps = (reduxState) => ({ reduxState });
+
+export default connect(reduxStateToProps)(SpeedControl);
--- src/index.js
+++ src/index.js
@@ -6,8 +6,19 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux';
 
 // put your reducer here!
+const firstReducerInitialState = 0;
+
+const myReducer = (state = firstReducerInitialState, action) => {
+   console.log('in myReducer:',  action)
+    if (action.type === 'increaseSpeed'){
+        state += action.payload;
+    } else if (action.type === 'decreaseSpeed'){
+        state -= action.payload;
+    }
+    return state;
+}
 
 // use reducer in store
-const storeInstance = createStore(  );
+const storeInstance = createStore( myReducer );
 
 ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
